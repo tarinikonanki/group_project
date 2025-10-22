5f037927b6ea8b680e5a6456103ccd2aca0cbe2a
tarinikonanki <tarini.konanki@gmail.com>
2025-10-22 22:59:28 +0000

    Finished most of problem 4

--- problem4.py
+++ problem4.py
@@ -6,3 +6,30 @@ answer[i] == "Buzz" if i is divisible by 5.
 answer[i] == i (as a string) if none of the above conditions are true.
 
 '''
+
+#Understand
+#summary: based on the integer, check if it follows certain standards, and if it meets those standards, set the answer array to that value
+#input: integer (basically the number/length of array)
+#output: string array
+#edge cases: if input is not int
+#questions: does it traverse n amount of times?
+
+#clues:
+#using array so will have use for loop / traversal, checking various conditions so will use if statements
+
+#assemble & solve
+
+#step 1: define function
+def answer_array(n):
+    #create array of strings
+    return_array = []
+    #create for loop to run n times
+    for i in range(n):
+        #check various conditions
+        if i % 3 and i % 5:
+            answer[i] = "FizzBuzz"
+        elif i % 3:
+            answer[i] = "Fizz"
+        elif i % 5:
+            answer[i] = "Buzz"
+               
