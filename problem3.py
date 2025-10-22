a4e0f6313aa690bea317a9d99be6d3ad7baa0b1c
nw6417 <nattygwa@gmail.com>
2025-10-22 23:00:08 +0000

    I finished problem 3

--- problem3.py
+++ problem3.py
@@ -1,6 +1,10 @@
-#problem: write a function to check if a number is even
-'''
-Example:
-Input: 4
-Output: True
-'''
+# U: Check if number is even
+# C: I will use mod
+def is_even(n): #A- I will be using mod division to get the remainder and if it equals 0 it will be true, if else it will be false
+    if n % 2 == 0:
+        return True
+    else: #S
+        return False
+# E- evaluate
+print(is_even(4))
+print(is_even(5))
